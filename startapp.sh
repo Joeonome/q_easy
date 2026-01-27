7d44361637b07d5ccf123f2f7bb41ea51ce585a6
joeonome <joseph.onumeguolor@gmail.com>
2026-01-27 20:14:00 +0100

    add startapp

---
+++ startapp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e  # exit immediately if a command fails
+
+sudo apt update && sudo apt upgrade -y && sudo apt install nginx -y
+sudo apt install certbot python3-certbot-nginx -y
+
+git clone https://github.com/Joeonome/q_easy.git
+cd q_easy
+sudo apt install python3.12-venv -y
+
+python3.12 -m venv myenv
+source myenv/bin/activate
+pip install --no-cache-dir -r requirements.txt
+
+sudo npm install -g pm2
+pm2 start "streamlit run app.py" --name q-easy
+pm2 save
+pm2 startup
