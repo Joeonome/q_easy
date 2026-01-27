3c7d10153b7604aec79002cb6e7c57b8bc896689
joeonome <joseph.onumeguolor@gmail.com>
2026-01-27 20:59:09 +0100

    add startapp

--- startapp.sh
+++ startapp.sh
@@ -3,7 +3,7 @@ set -e  # exit immediately if a command fails
 
 sudo apt update && sudo apt upgrade -y && sudo apt install nginx -y
 sudo apt install certbot python3-certbot-nginx -y
-
+rm -fr q_easy
 git clone https://github.com/Joeonome/q_easy.git
 cd q_easy
 sudo apt install python3.12-venv -y
